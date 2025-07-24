83cf12d3e43d6f82cfa5b2146f3b74b17e10cba7
ChainsQueen.eth <146336747+ChainsQueenEth@users.noreply.github.com>
2025-07-24 18:32:51 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
+  base: '/web3dashboard/', // 👈 Add this line
   plugins: [
     tailwindcss(),
   ],
